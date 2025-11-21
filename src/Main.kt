c9c0f856cf7fdbd055fe08b2dd7b946489b6b592
Evanio Tech <evanioshark@gmail.com>
2025-11-21 08:22:10 -0300

    use generics

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,19 @@ class Customer
 
 class Contact(val id: Int , var email: String)//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class MultableStack<E>(vararg items: E) {
+    private val elements = items.toMutableList()
+    fun push(element: E) = elements.add(element)
+    fun peek(): E = elements.last()
+    fun pop(): E = elements.removeAt(elements.size - 1)
+    fun isEmpty(): Boolean = elements.isEmpty()
+    fun size() = elements.size
+
+    override fun toString() = "MultableStack(${elements.joinToString(", ")})"
+
+}
+
+
 fun main() {
     val name = "Matias"
     val customer = Customer()
@@ -19,5 +32,17 @@ fun main() {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
+    }
+
+    val stack = MultableStack(0.62, 3.14, 2.7)
+    stack.push(9.87)
+    println(stack)
+
+    println("peek(): ${stack.peek()}")
+    println(stack)
+
+    for (i in 1..stack.size()) {
+        println("pop(): ${stack.pop()}")
+        println(stack)
     }
 }
